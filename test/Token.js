885ca5186a3c9841dc5012d12da503d7f7c241a3
KUAgod <KUACryptoCurrency@gmail.com>
2022-08-11 22:36:22 -0400

    DONE Token Approvals

--- test/Token.js
+++ test/Token.js
@@ -104,10 +104,21 @@ describe("Token", function () {
     describe('Success', () => {
       it('Allocates an allowance for delegated token spending', async () => {
         expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount);
-      })
+      });
+
+      it('Emits a Approval event', async () => {
+        const event = result.events[0];
+        expect(event.event).to.equal('Approval');
+        const args = event.args;
+        expect(args.owner).to.equal(deployer.address);
+        expect(args.spender).to.equal(exchange.address);
+        expect(args.value).to.equal(amount);
+      });
 
     describe('Failure', () => {
-
+      it('Rejects invalid spenders', async () => {
+        await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+      })
     })
     })
   })
