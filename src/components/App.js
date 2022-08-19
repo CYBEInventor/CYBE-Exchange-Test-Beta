90af9f5c96daefaf131629b1857d06dab5cd8cb6
KUAgod <KUACryptoCurrency@gmail.com>
2022-08-19 14:56:49 -0400

    DONE Redux Part 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,43 @@
 import { useEffect } from "react";
-import { ethers } from 'ethers';
-import TOKEN_ABI from '../abis/Token.json';
-import "../App.css";
+// import { ethers } from 'ethers';
+// import TOKEN_ABI from '../abis/Token.json';
+import { useDispatch } from 'react-redux'
 import config from '../config.json';
+import { 
+  loadProvider, 
+  loadNetwork, 
+  loadAccount,
+  loadToken
+} from '../store/interactions';
 
 function App() {
+const dispatch = useDispatch();
 
   const loadblockchainData = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    console.log(accounts[0]);
-
+    // const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    // console.log(accounts[0]);
+    // MOVED ACCOUNTS OVER TO INTERACTIONS
+    const account = await loadAccount(dispatch);
+    console.log(account)
     // Connect Ethers to blockchain .. connect to blockchain
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // dispatch({ type: 'PROVIDER_LOADED', connection: provider })
+    // MOVED PROVIDER OVER TO INTERACTIONS.JS
+    const provider = loadProvider(dispatch);
     // const network = await provider.getNetwork();
     // console.log(network.chainId)
     //  OR
-    const { chainId } = await provider.getNetwork();
-    console.log(chainId)
+    // const { chainId } = await provider.getNetwork();
+    // console.log(chainId)
+    // MOVED CHAINID OVER TO INTERACTIONS.JS
+    const chainId = await loadNetwork(provider, dispatch);
 
-    console.log()
-    
-    // Token Smart Cotract
-    const token = new ethers.Contract(config[chainId].Dapp.address, TOKEN_ABI, provider)
-    console.log(token.address);
-    const symbol = await token.symbol();
-    // ERROR HERE NOT GETTING THE SYMBOL.. mainly because im calling a address that isnt a contract
-    console.log(symbol);
+    // Token Smart Contract .. NOW MOVED TO REDUX: interactions.js, than reducers.js, than back here
+    // const token = new ethers.Contract(config[chainId].Dapp.address, TOKEN_ABI, provider)
+    // console.log(token.address);
+    // const symbol = await token.symbol();
+    // console.log(symbol);
+    await loadToken(provider, config[chainId].Dapp.address, dispatch);
   }
 
   useEffect(() => {
